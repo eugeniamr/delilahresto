73a93a523ee7a4e549518a7c45e27de4bf7fe650
Eugenia <68877151+eugeniamr@users.noreply.github.com>
2020-09-21 21:25:56 -0300

    Create products.js

---
+++ models/products.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, type) => {
+    return sequelize.define('products', {
+        id: {
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        product_name: type.STRING,
+        product_price: type.INTEGER,
+        product_photo: type.STRING(500)
+    })
+};
